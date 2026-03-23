ed9ce8d1b2199764fe3ea609c4daffea4c4caada
matthewthomasbeck <matthewthomasbeck@gmail.com>
2026-03-23 14:43:08 -0500

    Initial commit for mecanum functions (based off of what I know we will need, definitely will expand though)

---
+++ movement/mecanum.py
@@ -0,0 +1,60 @@
+##################################################################################
+# Copyright (c) 2026 Vending Machine Robot                                       #
+#                                                                                #
+# Licensed under the Creative Commons Attribution-NonCommercial 4.0              #
+# International (CC BY-NC 4.0). Personal and educational use is permitted.       #
+# Commercial use by companies or for-profit entities is prohibited.              #
+##################################################################################
+
+
+
+
+
+############################################################
+############### IMPORT / CREATE DEPENDENCIES ###############
+############################################################
+
+
+########## IMPORT DEPENDENCIES ##########
+
+
+########## CREATE DEPENDENCIES ##########
+
+
+
+
+
+###################################################
+############### MECANUM DRIVE LOGIC ###############
+###################################################
+
+
+########## FORWARD LOGIC ##########
+
+def forward(intensity):
+
+
+########## BACKWARD LOGIC ##########
+
+def backward(intensity):
+
+
+########## SHIFT LEFT LOGIC ##########
+
+def shift_left(intensity):
+
+
+########## SHIFT RIGHT LOGIC ##########
+
+def shift_right(intensity):
+
+
+########## ROTATE LEFT LOGIC ##########
+
+def rotate_left(intensity):
+
+
+########## ROTATE RIGHT LOGIC ##########
+
+def rotate_right(intensity):
+
