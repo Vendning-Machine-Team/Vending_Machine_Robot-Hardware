69ad2593dbe2b0f6362004ebf35ff004a1dcecea
matthewthomasbeck <matthewthomasbeck@gmail.com>
2026-03-23 14:51:25 -0500

    Filled out functions based on research of 45 degree mecanum wheels, will add more so we can fully utilize mecanum drive's power

--- movement/mecanum.py
+++ movement/mecanum.py
@@ -17,8 +17,9 @@
 
 ########## IMPORT DEPENDENCIES ##########
 
+##### import motor functions #####
 
-########## CREATE DEPENDENCIES ##########
+from utilities.motors import move_motor
 
 
 
@@ -32,29 +33,59 @@
 ########## FORWARD LOGIC ##########
 
 def forward(intensity):
+    # forward:
+    # fl counterclockwise, fr clockwise, bl counterclockwise, br clockwise
+    move_motor('FL', 'counterclockwise', intensity)
+    move_motor('FR', 'clockwise', intensity)
+    move_motor('BL', 'counterclockwise', intensity)
+    move_motor('BR', 'clockwise', intensity)
 
 
 ########## BACKWARD LOGIC ##########
 
 def backward(intensity):
+    # backward is forward reversed
+    move_motor('FL', 'clockwise', intensity)
+    move_motor('FR', 'counterclockwise', intensity)
+    move_motor('BL', 'clockwise', intensity)
+    move_motor('BR', 'counterclockwise', intensity)
 
 
 ########## SHIFT LEFT LOGIC ##########
 
 def shift_left(intensity):
+    # strafe left
+    move_motor('FL', 'clockwise', intensity)
+    move_motor('FR', 'clockwise', intensity)
+    move_motor('BL', 'counterclockwise', intensity)
+    move_motor('BR', 'counterclockwise', intensity)
 
 
 ########## SHIFT RIGHT LOGIC ##########
 
 def shift_right(intensity):
+    # strafe right is strafe left reversed
+    move_motor('FL', 'counterclockwise', intensity)
+    move_motor('FR', 'counterclockwise', intensity)
+    move_motor('BL', 'clockwise', intensity)
+    move_motor('BR', 'clockwise', intensity)
 
 
 ########## ROTATE LEFT LOGIC ##########
 
 def rotate_left(intensity):
+    # rotate left in place
+    move_motor('FL', 'clockwise', intensity)
+    move_motor('FR', 'clockwise', intensity)
+    move_motor('BL', 'clockwise', intensity)
+    move_motor('BR', 'clockwise', intensity)
 
 
 ########## ROTATE RIGHT LOGIC ##########
 
 def rotate_right(intensity):
-
+    # rotate right in place
+    move_motor('FL', 'counterclockwise', intensity)
+    move_motor('FR', 'counterclockwise', intensity)
+    move_motor('BL', 'counterclockwise', intensity)
+    move_motor('BR', 'counterclockwise', intensity)
