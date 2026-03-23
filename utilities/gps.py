d3a92f1a49d5c7303960dba20509ff38847c1026
matthewthomasbeck <matthewthomasbeck@gmail.com>
2026-03-23 15:41:39 -0500

    Set up bare-bones gps module (will work on later)

---
+++ utilities/gps.py
@@ -0,0 +1,35 @@
+##################################################################################
+# Copyright (c) 2026 Vending Machine Robot                                       #
+#                                                                                #
+# Licensed under the Creative Commons Attribution-NonCommercial 4.0              #
+# International (CC BY-NC 4.0). Personal and educational use is permitted.       #
+# Commercial use by companies or for-profit entities is prohibited.              #
+##################################################################################
+
+
+
+
+
+############################################################
+############### IMPORT / CREATE DEPENDENCIES ###############
+############################################################
+
+
+########## IMPORT DEPENDENCIES ##########
+
+
+########## CREATE DEPENDENCIES ##########
+
+
+
+
+
+##############################################
+############### INITIALIZE GPS ###############
+##############################################
+
+
+########## INITIALIZE GPS ##########
+
+def initialize_gps():
+    pass
