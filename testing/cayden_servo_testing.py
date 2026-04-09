2a443ecd1df89bb1c6801279ef492d8947a36700
Cayden Hutcheson <Sixtyfourandten@gmail.com>
2026-04-09 14:43:07 -0500

    Testing Serial Import

--- testing/cayden_servo_testing.py
+++ testing/cayden_servo_testing.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import time
+import serial
 
 # Add parent directory to path so we can import from utilities
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
