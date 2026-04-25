0db500aa6e4bf6f7b46272cef262969ad5f27d00
Tri Nguyen <Tringuyen7379@gmail.com>
2026-04-25 17:31:36 -0500

    fixed threshold

--- utilities/config.py
+++ utilities/config.py
@@ -65,10 +65,10 @@ PERSON_DETECTION_CONFIG = {
 
 PERSON_APPROACH_CONFIG = {
     'FRAME_WIDTH': 640,         # must match CAMERA_CONFIG['WIDTH']
-    'STOP_AREA': 42000,         # stop when person's box area reaches this threshold
+    'STOP_AREA': 50000,         # stop when person's box area reaches this threshold
     'DEADBAND': 92,             # horizontal pixel deadband around frame center
     'INTENSITY': 5,             # default motor intensity during approach
-    'SLOWDOWN_AREA': 32000,     # start slowing down when box area reaches this threshold
+    'SLOWDOWN_AREA': 35000,     # start slowing down when box area reaches this threshold
     'SLOW_INTENSITY': 2,        # reduced intensity while inside slowdown zone
 }
 
